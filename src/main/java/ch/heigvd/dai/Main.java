416457a9e9be7f2366156d3f7a6e5c50bfa511a7
olinb <olin.bourquin@gmail.com>
2023-11-07 14:28:55 +0100

    setup main

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -8,24 +8,21 @@ import picocli.CommandLine.Parameters;
 import java.io.File;
 import java.util.concurrent.Callable;
 
-@Command(name = "replaceWord", mixinStandardHelpOptions = true, version = "replaceWord 0.2",
-        description = "Replace all occurence of specific work in a file text and create new file")
-class ReplaceWord implements Callable<Integer>{
-    @Option(names = {"-i", "--if","--inputFile"}, paramLabel = "ARCHIVE",
-            description = "input file whose the specific word will be searched")
-    //private File inputFile;
-    private String inputFile;
+@Command(name = "Blackjack", mixinStandardHelpOptions = true, version = "Blackjack 0.1",
+        description = "host a Blackjack server")
+class Blackjack implements Callable<Integer>{
 
-    @Option(names = {"--ie", "--inputEncoding"}, defaultValue = "UTF-8", description = "Encoding of the input file")
-    private String inputEncoding;
+    @Command(name = "server")
+    public Integer prepare() {
+        System.out.println("Hello World ! (server)");
+        return 0;
+    }
 
-    @Option(names = {"-o", "--of", "--outputFile"}, paramLabel = "FILE",
-            description = "output file whose will be created with the word replaced")
-    //private File outputFile;
-    private String outputFile;
-
-    @Option(names = {"--oe", "--outputEncoding"}, defaultValue = "UTF-8", description = "Encoding of the output file")
-    private String outputEncoding;
+    @Command(name = "client")
+    public Integer process() {
+        System.out.println("Hello World ! (client)");
+        return 0;
+    }
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
     private boolean helpRequested = false;
@@ -33,24 +30,16 @@ class ReplaceWord implements Callable<Integer>{
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Display version info")
     private boolean versionRequested = false;
 
-    @Option(names = {"-s", "--sw", "--searchedWord"}, paramLabel = "SEARCH",
-    description = "The searched word will be replaced")
-    private String searchedWord;
-
-    @Option(names = {"-r", "--rw", "--replacedWord"}, paramLabel = "REPLACE",
-    description = "The remplacement word")
-    private String replacedWord;
-
     @Override
-    public Integer call() throws Exception{
-        ch.heigvd.dai.string.ReplaceWord.replaceInFile(inputFile, outputFile, searchedWord, replacedWord);
+    public Integer call() {
+        System.out.println("Subcommand needed: 'client' or 'server'");
         return 0;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        CommandLine commandLine = new CommandLine(new ReplaceWord());
+        CommandLine commandLine = new CommandLine(new Blackjack());
         if(commandLine.isUsageHelpRequested()){
             commandLine.usage(System.out);
             return;
